39995319851cd87525cb5ce60891f24ccb4c9dbd
bogus <boguslawski.m.j@gmail.com>
2024-11-20 11:56:24 +0100

    fix bugs

--- vision_models.py
+++ vision_models.py
@@ -4,7 +4,8 @@ import torch.nn.functional as F
 from torchvision.transforms import v2
 
 
-def conv_block(in_channels, out_channels, kernel_size=3, stride=1, padding=1, if_transpose=False, if_pool=False,
+def conv_block(in_channels, out_channels, kernel_size=3, stride=1, padding=1, if_norm=True, if_transpose=False,
+               if_pool=False,
                device='cpu'):
     if if_transpose:
         block = [nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
@@ -12,7 +13,8 @@ def conv_block(in_channels, out_channels, kernel_size=3, stride=1, padding=1, if
     else:
         block = [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
                            device=device)]
-    block.append(nn.InstanceNorm2d(out_channels, device=device))
+    if if_norm:
+        block.append(nn.InstanceNorm2d(out_channels, device=device))
     block.append(nn.GELU())
     if if_pool:
         block.append(nn.MaxPool2d(2))
@@ -27,9 +29,11 @@ class Residual2DBlock(nn.Module):
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
-        self.conv_layer0 = nn.Conv2d(in_channel, in_channel*times_channel, kernel_size, stride, padding, device=device)
+        self.conv_layer0 = nn.Conv2d(in_channel, in_channel * times_channel, kernel_size, stride, padding,
+                                     device=device)
         self.norm0 = nn.InstanceNorm2d(in_channel * times_channel)
-        self.conv_layer1 = nn.Conv2d(in_channel * times_channel, in_channel, kernel_size, stride, padding, device=device)
+        self.conv_layer1 = nn.Conv2d(in_channel * times_channel, in_channel, kernel_size, stride, padding,
+                                     device=device)
         self.norm1 = nn.InstanceNorm2d(in_channel)
 
     def forward(self, x):
@@ -53,6 +57,7 @@ class SimpleGenerator(nn.Module):
     gen_strides = [2, 2, 2, 4]
     gen_paddings = [0, 1, 1, 2]
     '''
+
     def __init__(self, latent_dim, dims, kernel_sizes, strides, paddings, new_size, if_res=True, device='cpu'):
         super(SimpleGenerator, self).__init__()
         self.latent_dim = latent_dim
@@ -67,14 +72,14 @@ class SimpleGenerator(nn.Module):
         blocks = []
         for i in range(self.n_times):
             blocks.append(conv_block(dims[i],
-                                     dims[i+1],
+                                     dims[i + 1],
                                      kernel_size=kernel_sizes[i],
                                      stride=strides[i],
                                      padding=paddings[i],
                                      if_transpose=True,
                                      device=device))
             if if_res[i]:
-                blocks.append(Residual2DBlock(dims[i+1], device=device))
+                blocks.append(Residual2DBlock(dims[i + 1], device=device))
 
         self.blocks = nn.Sequential(*blocks)
         self.out_conv = nn.Conv2d(dims[-1], 3, kernel_size=3, stride=1, padding=1, device=device)
@@ -100,33 +105,40 @@ class SimpleDiscriminator(nn.Module):
     disc_paddings = [1, 1, 1, 1]
     disc_if_pools = [True, False, True, False]
     '''
-    def __init__(self, dims, kernel_sizes, strides, paddings, if_pools, device='cpu'):
+
+    def __init__(self, dims, kernel_sizes, strides, paddings, if_pools, if_res=True, device='cpu'):
         super(SimpleDiscriminator, self).__init__()
         self.input_norm = nn.InstanceNorm2d(3, device=device)
         self.device = device
         self.n_times = len(kernel_sizes)
         blocks = []
         dims = [3, ] + dims
-        for i in range(self.n_times-1):
+        if_norm = True
+        if if_res:
+            if_res = [True for _ in range(self.n_times)]
+        for i in range(self.n_times):
+            if i == (self.n_times - 1):
+                if_norm = False
             blocks.append(conv_block(dims[i],
-                                     dims[i+1],
+                                     dims[i + 1],
                                      kernel_size=kernel_sizes[i],
                                      stride=strides[i],
                                      padding=paddings[i],
+                                     if_norm=if_norm,
                                      if_pool=if_pools[i],
                                      device=device))
+            if if_res[i] & if_norm:
+                blocks.append(Residual2DBlock(dims[i + 1], device=device))
         self.block = nn.Sequential(*blocks)
-        self.out_conv = nn.Conv2d(dims[-1], 1,
-                                  kernel_size=kernel_sizes[-1],
-                                  stride=strides[-1],
-                                  padding=paddings[-1],
-                                  device=device)
         self.flatten = nn.Flatten()
-        #self.dense = nn.Linear() ## tu dodawać
+        self.dense = nn.Linear(dims[-1], 1024, device=device)  ## tu dodawać
+        self.out_dense = nn.Linear(1024, 1, device=device)  ## tu dodawać
 
     def forward(self, x):
         x = self.input_norm(x)
         x = self.block(x)
-        x = self.out_conv(x)
-        x = x.view(-1, 1)
+        x = self.flatten(x)
+        x = self.dense(x)
+        x = F.gelu(x)
+        x = self.out_dense(x)
         return x.to(self.device)
